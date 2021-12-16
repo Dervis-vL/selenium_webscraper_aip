55067f7df0095a569a8c3aba731aefdab68e2288
Dervis D. van Leersum <dervis.vanleersum@gmail.com>
2021-12-16 13:47:08 +0100

    added driver arguments

--- download_knop.py
+++ download_knop.py
@@ -46,8 +46,10 @@ path = os.path.join(folder, batch_name)
 # set chrome options
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--window-size=1400,600')
+chrome_options.add_argument('--window-size=1366,768')
 # chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--start-maximized')
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 # add directory preference
 prefs = {}

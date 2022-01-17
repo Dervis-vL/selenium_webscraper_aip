a09331a6cfd4344a4ae72028c1d8b80457093920
Dervis D. van Leersum <dervis.vanleersum@gmail.com>
2022-01-17 13:42:55 +0100

    Added single object and excel download function

--- START.py
+++ START.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-from datetime import datetime
+from datetime import datetime, date
 import re, os, shutil
 
 # DOCSTRING
@@ -24,8 +24,15 @@ NOTE:
 check the webdriver version before running this script. 
 Make sure the driver has the same name and version as the browser of the user.
 
-Default browser and version:    Chrome 95
-Default driver:                 chromedriver.exe
+Default browser and version:    Chrome; version 95
+Default driver:                 chromedriver.exe (for version 95)
+
+Check your browser version: 'help' -->  'About Google Chrome'
+
+If the version differs from the default version used by this tool, than download the driver for the verion used by your machine here:
+https://chromedriver.chromium.org/downloads
+
+Replace the driver with the correct chromedriver.exe in the driver folder
 """
 
 # start timing
@@ -39,39 +46,34 @@ folder = input("Input directory for batch destination: ")
 driver_url = "https://aip.amsterdam.nl"
 
 #TODO: add function to select single bru instead of full batch
-# # single download or total download
-# choose_options = input("Do you want to download the whole batch?: (Y/N)\n")
-# if choose_options.upper() == "Y":
-#     single_download = False
-# elif choose_options.upper() == "N":
-#     single_download = True
-# else:
-#     print("Input option was incorrect, restart program.")
-#     exit()
+# single download or total download
+choose_options = input("Do you want to download the whole batch?: (Y/N)\n")
+if choose_options.upper() == "Y":
+    single_download = False
+elif choose_options.upper() == "N":
+    single_download = True
+else:
+    print("Input option was incorrect, restart program.")
+    exit()
 
-# # create batch folder
-# if single_download:
-#     single_bru = input("Input full objectnumber: ")
-#     single_bru = single_bru.lower()
-    # single_download_func(single_bru)
-# else:
-#     full_batch_download_func()
-
-
-# check and create batch folder 
+# check and create batch folder with error handeling for duplicates
+dupl = 0
+batch_name = "Batch " + str(batch)
 for file in os.listdir(folder):
-    batch_name = "Batch " + str(batch)
     if batch_name in file:
-        print("\nA file with the requested batch number already exists. Please remove.\n")
-        exit()
-    else:
-        path = os.path.join(folder, batch_name)
+        dupl += 1
+
+today = date.today()
+d = today.strftime("%d-%m-%y")
+
+batch_name = batch_name + "_v" + str(dupl) + "_" + str(d)
+path = os.path.join(folder, batch_name)
 
 # set chrome options
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1366,768')
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--start-maximized')
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -83,7 +85,7 @@ prefs["download.default_directory"]=path
 chrome_options.add_experimental_option("prefs", prefs)
 
 # connect to driver with selenium
-PATH = 'chromedriver.exe'
+PATH = 'driver//chromedriver.exe'
 driver = webdriver.Chrome(executable_path=PATH, options=chrome_options)
 driver.get(driver_url)
 
@@ -185,16 +187,119 @@ if len(regex_tables) == 0:
         """)
     exit()
 
-# loop through bru's and collect data
-count = 1
-for bru in regex_tables:
-    # remove whitespace
-    bru = bru.rstrip()
+# function tp loop through all bru's and collect data
+def all_data(regex_tables, driver):
+    count = 1
+    for bru in regex_tables:
+        # remove whitespace
+        bru = bru.rstrip()
+        # go to specific bru info page
+        bru_info_tag = "//tbody/tr[" + str(count) + "]/td[1]"
+        bru_data = find_by_xpath(bru_info_tag, driver)
+        bru_data.click()
+        
+        # assign one window
+        window_one = driver.window_handles[0]
+
+        # open DMS
+        dms_tag = "/html/body/span[1]/div[2]/div[1]/section[1]/header[1]/div[2]/a[1]"
+        find_dms = find_by_xpath(dms_tag, driver)
+        find_dms.click()
+
+        # assign and handle window two
+        window_dms = driver.window_handles[1]
+        driver.switch_to.window(window_dms)
+
+        # show all files in table
+        select_all_tag = '//select[@name="documents_length"]/option[text()="All"]'
+        select_all = find_by_xpath(select_all_tag, driver)
+        select_all.click()
+
+        #TODO: remove sleep() and make a better solution
+        sleep(3)
+
+        # fetch page html content and find all hyperlinks with regex
+        html_content = driver.page_source
+        regex_code = 'href="/documents/download/document/(.*?)"'
+        regex_hyperlinks = re.findall(regex_code, str(html_content))
+
+        # make folder for each bru in batch directory
+        path_bru = os.path.join(path, bru)
+        os.mkdir(path_bru)
+
+        # download all data
+        for file in regex_hyperlinks:
+            base = "https://bmidms.amsterdam.nl/documents/download/document/"
+            total_link = os.path.join(base, file) 
+
+            # get file
+            driver.get(total_link)
+
+        # wait till downloads ready
+        download_check = os.listdir(path)
+        while any(".crdownload" in file for file in download_check):
+            sleep(1)
+            download_check = os.listdir(path)
+        
+        # move all files to bru specific directory
+        move_list = os.listdir(path)
+        for files in move_list:
+            fpath = os.path.join(path, files)
+            if not os.path.isdir(fpath):
+                if fpath.endswith(".tmp"):
+                    pass
+                else:
+                    shutil.move(fpath, path_bru)
+
+        #close window_dms
+        driver.close()
+
+        #assign window_one
+        driver.switch_to.window(window_one)
+
+        #go back one
+        driver.back()
+
+        # print update on download progress
+        print("(" + str(count) + "/" + str(len(regex_tables)) + "): " + "From " + str(batch_name) + " and object " + str(bru) + " there where " + str(len(regex_hyperlinks)) + " files downloaded.")
+        count += 1
+
+    #TODO: download 'Excel report'
+    excel_export_tag = '//*[@id="objectlist"]/div[1]/div/a[2]'
+    excel_report = find_by_xpath(excel_export_tag, driver)
+    excel_report.click()
+
+# function to get single bru from aip site
+def single_data(regex_tables, driver):
+    count = 1
+    for bru in regex_tables:
+        # remove whitespace
+        bru = bru.rstrip()
+
+        # print all bru's found for selection
+        sum_all_bru = str(count) + " :   " + str(bru)
+        print(sum_all_bru)
+        count += 1
+    
+    # get input that specifies wich bru to find
+    input_test = True
+    while input_test:
+        specific_bru = input("\nInput the integer number from list to select object: ")
+
+        # check input
+        try:
+            if type(specific_bru) == int:
+                input_test = False
+            else:
+                print("\nInput is not recognized, specify the number from the object as listed above.")
+        except:
+            print("ERROR with the input. Try again and specify the number from the object as listed above.")
+
     # go to specific bru info page
-    bru_info_tag = "//tbody/tr[" + str(count) + "]/td[1]"
+    bru_info_tag = "//tbody/tr[" + str(specific_bru) + "]/td[1]"
     bru_data = find_by_xpath(bru_info_tag, driver)
     bru_data.click()
-    
+        
     # assign one window
     window_one = driver.window_handles[0]
 
@@ -220,7 +325,7 @@ for bru in regex_tables:
     regex_code = 'href="/documents/download/document/(.*?)"'
     regex_hyperlinks = re.findall(regex_code, str(html_content))
 
-    # make folder for each bru in batch directory
+    # make folder for bru in batch directory
     path_bru = os.path.join(path, bru)
     os.mkdir(path_bru)
 
@@ -237,10 +342,10 @@ for bru in regex_tables:
     while any(".crdownload" in file for file in download_check):
         sleep(1)
         download_check = os.listdir(path)
-    
+        
     # move all files to bru specific directory
     move_list = os.listdir(path)
-    for files in os.listdir(path):
+    for files in move_list:
         fpath = os.path.join(path, files)
         if not os.path.isdir(fpath):
             if fpath.endswith(".tmp"):
@@ -256,8 +361,16 @@ for bru in regex_tables:
 
     #go back one
     driver.back()
-    print("(" + str(count) + "/" + str(len(regex_tables)) + "): " + "From " + str(batch_name) + " and object " + str(bru) + " there where " + str(len(regex_hyperlinks)) + " files downloaded.")
-    count += 1
+
+    # print update on download progress
+    print("(" + str(specific_bru) + "/" + str(len(regex_tables)) + "): " + "From " + str(batch_name) + " and object " + str(bru) + " there where " + str(len(regex_hyperlinks)) + " files downloaded.")
+
+
+# make selection for complete bru download or single bru selection
+if single_download == False:
+    all_data(regex_tables, driver)
+elif single_download == True:
+    single_data(regex_tables, driver)
 
 # delete user login data
 del user

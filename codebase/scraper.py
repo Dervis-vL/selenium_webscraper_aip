4d34e789674ba3bd8a8a74b93f8c45b1f7030f91
Dervis D. van Leersum <dervis.vanleersum@gmail.com>
2022-04-19 21:30:21 +0200

    Back to headless mode

--- codebase/scraper.py
+++ codebase/scraper.py
@@ -80,7 +80,7 @@ def input_scraper(batch, folder):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window-size=1366,768')
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--start-maximized')
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
